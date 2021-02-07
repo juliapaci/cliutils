7ce435a6508bc8c079de69f07d7b6847c8f36160
fabianpaci <fabianpaci@gmail.com>
2021-02-07 17:21:42 +1100

    etst

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
+module.exports = function () {
+  const vorpal = require("vorpal")();
+  vorpal.delimiter().show();
+
+  // const goto = function (url) {
+  //   vorpal
+  //     .command("goto", "goes to a url with your default browser")
+  //     .action(function (args, callback) {
+  //       this.log("goto");
+  //       callback();
+  //     });
+  // };
+
+  // const repl = function () {
+  //   vorpal
+  //     .command("repl", "do maths -original")
+  //     .mode("repl")
+  //     .delimiter("repl:")
+  //     .action(function (command, callback) {
+  //       this.log(eval(command));
+  //     });
+  // };
+
+  // const sql = function () {
+  //   vorpal
+  //     .command("sql", "directly enter arbitrary SQL. -original")
+  //     .mode("sql")
+  //     .delimiter("sql:")
+  //     .init(function (args, callback) {
+  //       this.log(
+  //         "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
+  //       );
+  //       callback();
+  //     })
+  //     .action(function (command, callback) {
+  //       var self = this;
+  //       app.query(command, function (res) {
+  //         self.log(res);
+  //         callback();
+  //       });
+  //     });
+  // };
+};
 const vorpal = require("vorpal")();
+vorpal.delimiter().show();
 
 vorpal
   .command("goto", "goes to a url with your default browser")
@@ -7,6 +51,32 @@ vorpal
     callback();
   });
 
-vorpal.command();
+vorpal
 
-vorpal.delimiter().show();
+  .mode("repl")
+  .description("d o maths -original")
+  .delimiter("repl:")
+  .action(function (command, callback) {
+    this.log(eval(command));
+  });
+
+vorpal
+
+  .mode("sql")
+  .description("directly enter arbitrary SQL. -original")
+  .delimiter("sql:")
+  .init(function (args, callback) {
+    this.log(
+      "Welcome to SQL mode.\nYou can now directly enter arbitrary SQL commands. To exit, type `exit`."
+    );
+    callback();
+  })
+  .action(function (command, callback) {
+    var self = this;
+    app.query(command, function (res) {
+      self.log(res);
+      callback();
+    });
+  });
+
+// vorpal.mode().description("goes to home page");
